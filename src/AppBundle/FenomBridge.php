ed57aa92975b3eb09ecf9b77689fd89b098c06f7
exarchik <exarchik@gmail.com>
2017-10-16 12:32:41 +0300

    edit fenom bridge

--- src/AppBundle/FenomBridge.php
+++ src/AppBundle/FenomBridge.php
@@ -7,11 +7,19 @@ use Fenom\Extra;
 
 class FenomBridge
 {
-     
+    
+    /**
+     * FenomBridge::render()
+     * 
+     * @param mixed $tpl
+     * @param mixed $options
+     * @return void
+     */
     function render($tpl,$options){
+        
         $fenom = Fenom\Extra::factory($tpl,$tpl,$options);
         
-        
+        return $fenom;
     }
     
 }
